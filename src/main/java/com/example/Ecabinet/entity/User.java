47127b2bfe67111892cce551768b72dbaa6dca59
tuantv <tuantv@rikkeisoft.com>
2020-07-21 17:08:02 +0700

    add entity

---
+++ src/main/java/com/example/Ecabinet/entity/User.java
@@ -0,0 +1,79 @@
+package com.example.Ecabinet.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String username;
+
+    private String password;
+
+    private String role;
+    
+    private String firstname;
+    
+    private String lastname;
+    
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public User() {
+        super();
+    }
+
+    public User(String username, String password, String role, String firstname, String lastname) {
+        super();
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+}
